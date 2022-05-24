deb8d28bcc9d863e47bcbc51195267ba36e21f79
jvcataquiz <jvcataquiz@gmail.com>
2022-05-24 15:21:08 +0800

    Course2: Coding Exercise 3.4 in Programming for Intermediate Users Using Python is Completed

---
+++ Programming for Intermediate Users Using Python/3.4Exercise.py
@@ -0,0 +1,9 @@
+create = open("newfile.txt", "w")
+create.write("It's a high level Programming Language")
+create = open("newfile.txt", "a")
+create.write("\n i try to finish this code for my future referrence")
+create.write("\n i try to finish this code for my future referrence")
+create.write("\n i try to finish this code for my future referrence")
+create = open("newfile.txt", "r")
+
+create.close()
