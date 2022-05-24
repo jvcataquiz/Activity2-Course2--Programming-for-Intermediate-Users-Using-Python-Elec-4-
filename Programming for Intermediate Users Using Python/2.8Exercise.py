d5b74582292cfdf46720a7ee4f4102659172c5e7
jvcataquiz <jvcataquiz@gmail.com>
2022-05-24 14:56:20 +0800

    Course2: Coding Exercise 2.8 in Programming for Intermediate Users Using Python is Completed

---
+++ Programming for Intermediate Users Using Python/2.8Exercise.py
@@ -0,0 +1,13 @@
+groceries = {"chicken": 8 , "apples": 6,
+             "cucumbers": 3, "oranges": 4 }
+print(groceries.pop("oranges"))
+
+
+speakers = {"Sir Rafeal": 54,"Ms. Joan": 33, "Ms. Dana": 67}
+print(speakers.keys())
+"passed"
+tryout= {"Carl": "passed","Quetin":"failed", "John Y.": "passed", "Peter": "failed"
+,"Max T.": "passed" , "Joseph": "passed", "Jone": "failed", "Jorge": "failed", "George": "passed"
+, "Ben": "passed", "Jerome":"passed", "Rick": "failed", "Max G.": "failed", "John P.":"failed", "Vince":"passed"}
+
+print(tryout.get("Jorge"))
