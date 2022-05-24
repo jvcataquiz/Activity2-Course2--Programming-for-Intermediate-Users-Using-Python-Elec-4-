c77aa3466c99807d111678186391802f02732fd9
jvcataquiz <jvcataquiz@gmail.com>
2022-05-24 16:14:44 +0800

    Course2: Coding Exercise 4.4  in Programming for Intermediate Users Using Python is Completed

---
+++ Programming for Intermediate Users Using Python/4.4Exercise.py
@@ -0,0 +1,4 @@
+import functionfile
+
+print(functionfile.sum(2,5))
+print(functionfile.prod(2,5))
---
+++ Programming for Intermediate Users Using Python/functionfile.py
@@ -0,0 +1,14 @@
+def sum(x, y):
+    return x + y
+def dif(x, y):
+    return x - y
+
+def prod(x, y):
+    return x * y
+
+def quo(x, y):
+    return x / y
+def floor(x, y):
+    return x // y
+def mod(x, y):
+    return x % y
